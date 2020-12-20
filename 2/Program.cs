2552ea8a9b8bdfb27b812e7ce7a52cb97adc2fb5
Johan Driessen <jd@approach.se>
2020-12-20 21:26:40 +0100

    Day 2, part 1

---
+++ 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var lines = File.ReadAllLines("input.txt");
+
+			int validLines = 0;
+			foreach(var line in lines)
+			{
+				validLines += IsValid(line);
+			}
+			Console.WriteLine($"Found {validLines} valid lines.");
+		}
+
+		static int IsValid(string line)
+		{
+			var min = int.Parse(line.Substring(0, line.IndexOf("-")));
+			var max = int.Parse(line.Substring(line.IndexOf("-")+1, line.IndexOf(" ")-line.IndexOf("-")-1));
+			var c = Convert.ToChar(line.Substring(line.IndexOf(":")-1, 1));
+			var pwd = line.Substring(line.IndexOf(":")+2);
+
+			var count = pwd.Where(x => x == c).Count();
+			return min <= count && count <= max ? 1 : 0;
+		}
+	}
+
+}
